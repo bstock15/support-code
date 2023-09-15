7770dffe4910e8c1bacb363c4f3488732f108b51
Marcus Miller <marcus04@iastate.edu>
2023-09-15 17:13:38 -0500

    Changes

--- parachute_calculations/parachute_calc.py
+++ parachute_calculations/parachute_calc.py
@@ -20,33 +20,38 @@ def Main():
     AftMotorTube = 3.14/g #slugs
     MotorWeightNoProp = (5.77 - 2.8)/g #slugs
 
+    #Parameters to determine successful values for variables
+    KE2_Limit = 75
+    DecentTime_Limit = 90
+    Drift20_Limit = 2500
+    VDrogue_Limit = 100
+    VMain_Limit = 15
+
     Section1 = NoseCone + PayloadTube + ForwardSwitchBand #slugs
     Section2 = DrogueTube + AvionicsBay + MainTube #slugs
     Section3 = ForwardMotorTube + AftMotorTube + AftSwitchBand + MotorWeightNoProp #slugs
 
     RocketWeightNoM = (Section1 + Section2 + Section3) #slugs
 
-    #This section creates matrices with drogue and main 
-    #parachute options
     #Parachute data taken from fruitychutes
     #Main parachutes = Iris Ultra Standard Parachutes
     #DrogueParachute = Elliptical/Compact Elliptical Parachutes
 
-    Drogue = [12, 15, 18, 24] #inches
     DrogueCd = [1.6, 1.6, 1.6, 1.6]
-    Main = [36, 48, 60, 72, 84, 96, 120] #inches
+    Drogue = [12, 15, 18, 24]
+    VDrogue = [0, 0, 0, 0] 
+    Main = [36, 48, 60, 72, 84, 96, 120]
     MainCd = [2.2, 2.2, 2.2, 2.2, 2.2, 2.2, 2.2]
+    VMain = [0, 0, 0, 0, 0, 0, 0]
+    
+    KE1 = [0, 0, 0, 0, 0, 0, 0]
+    KE2 = [0, 0, 0, 0, 0, 0, 0]
+    KE3 = [0, 0, 0, 0, 0, 0, 0]
+    
+    KE1V = [0, 0, 0, 0]
+    KE2V = [0, 0, 0, 0]
+    KE3V = [0, 0, 0, 0]
 
-    #Matrices to keep track of the data
-    Parachutes = n.zeros([len(Main), len(Drogue)])
-    VDrogue = n.zeros([len(Main), len(Drogue)])
-    VMain = n.zeros([len(Main), len(Drogue)])
-    KE1 = n.zeros([len(Main), len(Drogue)])
-    KE2 = n.zeros([len(Main), len(Drogue)])
-    KE3 = n.zeros([len(Main), len(Drogue)])
-    KE1V = n.zeros([len(Main), len(Drogue)])
-    KE2V = n.zeros([len(Main), len(Drogue)])
-    KE3V = n.zeros([len(Main), len(Drogue)])
     DecentTime = n.zeros([len(Main), len(Drogue)])
     Drift0 = n.zeros([len(Main), len(Drogue)]) 
     Drift5 = n.zeros([len(Main), len(Drogue)])
@@ -54,44 +59,38 @@ def Main():
     Drift15 = n.zeros([len(Main), len(Drogue)])
     Drift20 = n.zeros([len(Main), len(Drogue)])
 
-    #For loop to get data for all drogue and main configurations
-    for i in range (len(Main)):
-        for j in range (len(Drogue)):
-            #Parachutes[i,j] = Drogue[j], Main[i] 
-            Parachutes[i] = Main[i]
-            Parachutes[j] = Drogue[j]
+    for j in range (len(Main)): 
+        VMain[j] = n.sqrt((8 * RocketWeightNoM * g)/(n.pi * AirDensity * MainCd[j] * (Main[j]/12)**2))             
+        #Kinetic Energy Calculations
+        KE1[j] = (.5) * (Section1) * (VMain[j]**2) #lbf
+        KE2[j] = (.5) * (Section2) * (VMain[j]**2) #lbf #This is the important one
+        KE3[j] = (.5) * (Section3) * (VMain[j]**2) #lbf       
+        
+    for j in range (len(Drogue)):
+        VDrogue[j] = n.sqrt((8 * RocketWeightNoM * g)/(n.pi * AirDensity * DrogueCd[j] * (Drogue[j]/12)**2))
+        #Kinetic Energy Calculations
+        KE1V[j] = (.5) * (Section1) * (VDrogue[j]**2) #lbf
+        KE2V[j] = (.5) * (Section2) * (VDrogue[j]**2) #lbf #This is the important one
+        KE3V[j] = (.5) * (Section3) * (VDrogue[j]**2) #lbf
 
-            #Decent Velocities: Drogue & Main
-            VDrogue[i,j] = n.sqrt((8 * RocketWeightNoM * g)/(n.pi * AirDensity * DrogueCd[j] * (Drogue[j]/12)**2))
-            VMain[i,j] = n.sqrt((8 * RocketWeightNoM * g)/(n.pi * AirDensity * MainCd[i] * (Main[i]/12)**2))       
-            
-            #Kinetic Energy Calculations
-            KE1[i,j] = (.5) * (Section1) * (VMain[i,j]**2) #lbf
-            KE2[i,j] = (.5) * (Section2) * (VMain[i,j]**2) #lbf #This is the important one
-            KE3[i,j] = (.5) * (Section3) * (VMain[i,j]**2) #lbf
-            
-            #Kinetic energy calculations for the drogue
-            KE1V[i,j] = (.5) * (Section1) * (VDrogue[i,j]**2) #lbf
-            KE2V[i,j] = (.5) * (Section2) * (VDrogue[i,j]**2) #lbf #This is the important one
-            KE3V[i,j] = (.5) * (Section3) * (VDrogue[i,j]**2) #lbf
-            
-            #Decent Time Calculations
-            DecentTime[i,j] = ((Apogee - MainEjectHeight)/VDrogue[i,j]) + (MainEjectHeight/VMain[i,j]) #s
-            
-            #Drift Calculations
-            Drift0[i,j] = (0) * (DecentTime[i,j]) #ft 
-            Drift5[i,j] = (7 + 1/3) * (DecentTime[i,j]) #ft
-            Drift10[i,j] = (14 + 2/3) * (DecentTime[i,j]) #ft
-            Drift15[i,j] = (22) * (DecentTime[i,j]) #ft
-            Drift20[i,j] = (29 + 1/3) * (DecentTime[i,j]) #ft #This is the important one
+    for i in range(len(Main)):
+        for j in range (len(Drogue)):     
+                DecentTime[i,j] = ((Apogee - MainEjectHeight)/VDrogue[j]) + (MainEjectHeight/VMain[i]) #s       
+                #Drift Calculations
+                '''Drift0[i,j] = (0) * (DecentTime[i,j]) #ft 
+                Drift5[i,j] = (7 + 1/3) * (DecentTime[i,j]) #ft
+                Drift10[i,j] = (14 + 2/3) * (DecentTime[i,j]) #ft
+                Drift15[i,j] = (22) * (DecentTime[i,j]) #ft'''
+                Drift20[i,j] = (29 + 1/3) * (DecentTime[i,j]) #ft #This is the important one
 
     #Prints out all of the drogue-main configurations that meet the NASA handbook requirements
     print('\n')
     print('NASA Handbook Successful Parachute Configurations:')
     for i in range(len(Main)):
         for j in range(len(Drogue)):
-            if(KE2[i,j] < 75 and DecentTime[i,j] < 90 and Drift20[i,j] < 2500):
-                print('Drogue Parachute %d in and Main Parachute %d in\n', "Drogue:", Drogue[j], "Main:", Main[i])
+            if(KE2[j] < KE2_Limit and DecentTime[i,j] < DecentTime_Limit and Drift20[i,j] < Drift20_Limit):
+                data = n.array(["Drogue:", Drogue[j] , "Main:", Main[i]])
+                print(data)
 
     #Prints out all of the drogue-main parachute configurations that meet both
     #the NASA handbook requirements and CyLaunch's requirements 
@@ -101,35 +100,35 @@ def Main():
     print('CyLaunch Successful Parachute Configurations:')
     for i in range (len(Main)):
         for j in range (len(Drogue)):
-            if(KE2[i,j] < 75 and DecentTime[i,j] < 90 and Drift20[i,j] < 2500 and VDrogue[i,j] < 100 and VMain[i,j] < 15):
-                print('Drogue Parachute %d in and Main Parachute %d in:\n', "Drogue:", Drogue[j], "Main:", Main[i])
+            if(KE2[j] < KE2_Limit and DecentTime[i,j] < DecentTime_Limit and Drift20[i,j] < Drift20_Limit and VDrogue[j] < VDrogue_Limit and VMain[j] < VMain_Limit):
+                data2 = n.array(["Drogue:", Drogue[j] , "Main:", Main[i]])
+                print(data2)
 
-    graph_data(1, Drogue, VDrogue.T, 'Drogue Parachute Decent Velocities', 'Drogue Diameter (in)', 'Velocity (ft/s)')
+    graph_data(1, Drogue, VDrogue, Drogue, 1, 'Drogue Parachute Decent Velocities', 'Drogue Diameter (in)', 'Velocity (ft/s)')
 
-    graph_data(2, Main, VMain, 'Main Parachute Decent Velocities', 'Main Diameter (in)', 'Velocity (ft/s)')
+    graph_data(2, Main, VMain, Main, 3, 'Main Parachute Decent Velocities', 'Main Diameter (in)', 'Velocity (ft/s)')
 
-    graph_data(3, Main, KE1, 'Ground Hit Kinetic Energy of Section 1 for Main', 'Main Diameter (in)', 'Kinetic Energy (lbf)')
+    graph_data(3, Main, KE1, Main, 3, 'Ground Hit Kinetic Energy of Section 1 for Main', 'Main Diameter (in)', 'Kinetic Energy (lbf)')
 
-    graph_data(4, Main, KE2, 'Ground Hit Kinetic Energy of Section 2 for Main', 'Main Diameter (in)', 'Kinetic Energy (lbf)')
+    graph_data(4, Main, KE2, Main, 3, 'Ground Hit Kinetic Energy of Section 2 for Main', 'Main Diameter (in)', 'Kinetic Energy (lbf)')
 
-    graph_data(5, Main, KE3, 'Ground Hit Kinetic Energy of Section 3 for Main', 'Main Diameter (in)', 'Kinetic Energy (lbf)')
+    graph_data(5, Main, KE3, Main, 3, 'Ground Hit Kinetic Energy of Section 3 for Main', 'Main Diameter (in)', 'Kinetic Energy (lbf)')
 
-    graph_data(6, Drogue, KE1V.T, 'Ground Hit Kinetic Energy of Section 1 for Drogue', 'Drogue Diameter (in)', 'Kinetic Energy (lbf)')
+    graph_data(6, Drogue, KE1V, Drogue, 1, 'Ground Hit Kinetic Energy of Section 1 for Drogue', 'Drogue Diameter (in)', 'Kinetic Energy (lbf)')
 
-    graph_data(7, Drogue, KE2V.T, 'Ground Hit Kinetic Energy of Section 2 for Drogue', 'Drogue Diameter (in)', 'Kinetic Energy (lbf)')
+    graph_data(7, Drogue, KE2V, Drogue, 1, 'Ground Hit Kinetic Energy of Section 2 for Drogue', 'Drogue Diameter (in)', 'Kinetic Energy (lbf)')
 
-    graph_data(8, Drogue, KE3V.T, 'Ground Hit Kinetic Energy of Section 3 for Drogue', 'Drogue Diameter (in)', 'Kinetic Energy (lbf)')
+    graph_data(8, Drogue, KE3V, Drogue, 1, 'Ground Hit Kinetic Energy of Section 3 for Drogue', 'Drogue Diameter (in)', 'Kinetic Energy (lbf)')
 
-    graph_data2(9, Main, DecentTime, ('12','15','18','24'),'Decent Time for Different Configurations', 'Parachute Configuration', 'Time (s)')
+    graph_data2(9, Main, DecentTime, ('12','15','18','24'), 'Decent Time for Different Configurations', 'Main Diameter (in)', 'Time (s)')
 
-    graph_data2(10, Main, Drift20, ('12','15','18','24'),'20 mi/hr Drift for Different Configurations', 'Parachute Configuration', 'Drift (ft)')
-
-    print('\n')
-    print("Done")
-
-def graph_data(figure, x, y, title, xlabel, ylabel):
+    graph_data2(10, Main, Drift20, ('12','15','18','24'), '20 mi/hr Drift for Different Configurations', 'Main Diameter (in)', 'Drift (ft)')
+    
+def graph_data(figure, x, y, z, c, title, xlabel, ylabel):
     p.figure(figure)
-    p.plot(x, y)
+    p.bar(x, y, width = c)
+    p.xticks(z)
+    p.yticks(y)
     p.title(title)
     p.xlabel(xlabel)
     p.ylabel(ylabel)
@@ -139,7 +138,7 @@ def graph_data(figure, x, y, title, xlabel, ylabel):
 def graph_data2(figure, x, y, legend, title, xlabel, ylabel):
     p.figure(figure)
     p.plot(x, y)
-    p.legend(legend)
+    p.legend(legend, title = "Drogue Diameter (in)")
     p.title(title)
     p.xlabel(xlabel)
     p.ylabel(ylabel)
