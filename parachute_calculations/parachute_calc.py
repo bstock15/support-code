60eaa4dc75f2615eddfc3f95b03c6716391001c3
bdpope <bryan.pope.d@gmail.com>
2023-09-16 15:13:28 -0500

    Added review comments

--- parachute_calculations/parachute_calc.py
+++ parachute_calculations/parachute_calc.py
@@ -1,14 +1,25 @@
+#-----------------------------------------------------------------
+# rvw - put a high level description of the software in this file
+#-----------------------------------------------------------------
 import numpy as n
 from matplotlib import pyplot as p
 
 def Main():
-    #This section lays out the varibles that should be changed
+    #-------------------------------------------------------------
+    # Constants
+    #-------------------------------------------------------------
+    g = 32.2 #ft/s^2
+
+    #-------------------------------------------------------------
+    # Rocket Performance Parameters
+    #-------------------------------------------------------------
     Apogee = 4500 #ft
     MainEjectHeight = 550 #ft
     AirDensity = 0.00233546 #slug/ft^3 at 600 ft altitude
 
-    #This section is for rocket parameters
-    g = 32.2 #ft/s^2
+    #-------------------------------------------------------------
+    # Rocket Mass Parameters
+    #-------------------------------------------------------------
     NoseCone = 2.39/g #slugs
     PayloadTube = 3.37/g #slugs
     ForwardSwitchBand = 1.91/g #slugs
@@ -20,8 +31,10 @@ def Main():
     AftMotorTube = 3.14/g #slugs
     MotorWeightNoProp = (5.77 - 2.8)/g #slugs
 
-    #Parameters to determine successful values for variables
-    KE2_Limit = 75
+    #-------------------------------------------------------------
+    # Rocket Success Parameters
+    #-------------------------------------------------------------
+    KE2_Limit = 75 # rvw - Add units
     DecentTime_Limit = 90
     Drift20_Limit = 2500
     VDrogue_Limit = 100
@@ -33,9 +46,9 @@ def Main():
 
     RocketWeightNoM = (Section1 + Section2 + Section3) #slugs
 
-    #Parachute data taken from fruitychutes
-    #Main parachutes = Iris Ultra Standard Parachutes
-    #DrogueParachute = Elliptical/Compact Elliptical Parachutes
+    # Parachute data taken from fruitychutes
+    # Main parachutes = Iris Ultra Standard Parachutes
+    # DrogueParachute = Elliptical/Compact Elliptical Parachutes
 
     DrogueCd = [1.6, 1.6, 1.6, 1.6]
     Drogue = [12, 15, 18, 24]
@@ -59,6 +72,9 @@ def Main():
     Drift15 = n.zeros([len(Main), len(Drogue)])
     Drift20 = n.zeros([len(Main), len(Drogue)])
 
+    # rvw - take some time and tell me what each loop is doing in a comment
+    # rvw - it's good practice to use i as the first layer of a for loop and go down with
+    # each layer of nesting (i, j, k, etc).
     for j in range (len(Main)): 
         VMain[j] = n.sqrt((8 * RocketWeightNoM * g)/(n.pi * AirDensity * MainCd[j] * (Main[j]/12)**2))             
         #Kinetic Energy Calculations
@@ -77,20 +93,23 @@ def Main():
         for j in range (len(Drogue)):     
                 DecentTime[i,j] = ((Apogee - MainEjectHeight)/VDrogue[j]) + (MainEjectHeight/VMain[i]) #s       
                 #Drift Calculations
+                # rvw - unless it slows down the progam a ton, lets go ahead and get this data.
+                # we can cross refrence it with successful configurations to get more data
                 '''Drift0[i,j] = (0) * (DecentTime[i,j]) #ft 
                 Drift5[i,j] = (7 + 1/3) * (DecentTime[i,j]) #ft
                 Drift10[i,j] = (14 + 2/3) * (DecentTime[i,j]) #ft
                 Drift15[i,j] = (22) * (DecentTime[i,j]) #ft'''
                 Drift20[i,j] = (29 + 1/3) * (DecentTime[i,j]) #ft #This is the important one
 
-    #Prints out all of the drogue-main configurations that meet the NASA handbook requirements
+    # Prints out all of the drogue-main configurations that meet the NASA handbook requirements 3.3, 3.12, 3.11
     print('\n')
     print('NASA Handbook Successful Parachute Configurations:')
     for i in range(len(Main)):
         for j in range(len(Drogue)):
             if(KE2[j] < KE2_Limit and DecentTime[i,j] < DecentTime_Limit and Drift20[i,j] < Drift20_Limit):
                 data = n.array(["Drogue:", Drogue[j] , "Main:", Main[i]])
-                print(data)
+                print(data) # rvw - we'll want to print outside of the loop, otherwise we're printing the entire array
+                            # each time this if statement is hit
 
     #Prints out all of the drogue-main parachute configurations that meet both
     #the NASA handbook requirements and CyLaunch's requirements 
@@ -98,11 +117,12 @@ def Main():
 
     print('\n')
     print('CyLaunch Successful Parachute Configurations:')
+    # rvw - we now have an array of rockets that meet nasa configurations, why don't we iterate through that list instead of all rockets?
     for i in range (len(Main)):
         for j in range (len(Drogue)):
             if(KE2[j] < KE2_Limit and DecentTime[i,j] < DecentTime_Limit and Drift20[i,j] < Drift20_Limit and VDrogue[j] < VDrogue_Limit and VMain[j] < VMain_Limit):
                 data2 = n.array(["Drogue:", Drogue[j] , "Main:", Main[i]])
-                print(data2)
+                print(data2) # rvw - same as above
 
     graph_data(1, Drogue, VDrogue, Drogue, 1, 'Drogue Parachute Decent Velocities', 'Drogue Diameter (in)', 'Velocity (ft/s)')
 
@@ -123,6 +143,8 @@ def Main():
     graph_data2(9, Main, DecentTime, ('12','15','18','24'), 'Decent Time for Different Configurations', 'Main Diameter (in)', 'Time (s)')
 
     graph_data2(10, Main, Drift20, ('12','15','18','24'), '20 mi/hr Drift for Different Configurations', 'Main Diameter (in)', 'Drift (ft)')
+
+    # rvw - can we make a graph for each drift scenario for each successful rocket config?
     
 def graph_data(figure, x, y, z, c, title, xlabel, ylabel):
     p.figure(figure)
